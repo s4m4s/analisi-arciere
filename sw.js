f749790b67d08106f0cfd5e1ad29bd888a6957eb
s4m4s <58971592+s4m4s@users.noreply.github.com>
2025-12-31 09:13:16 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker v37
-const CACHE_NAME = 'analisi-arciere-v37';
+// Service Worker v38
+const CACHE_NAME = 'analisi-arciere-v38';
 const APP_FILES = ['./', './index.html', './app.html', './manifest.json', './logo-pol.png'];
 
 self.addEventListener('install', (e) => {
